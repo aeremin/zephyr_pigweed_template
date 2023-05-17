25800cad1ec2e3e16423e1c2a01f66edd52ff13e
Alexey Eremin <a.eremin.msu@gmail.com>
2023-05-17 23:48:40 +0200

    Actually enable logging and make sure that main() never finishes.

--- sample_app/main.cc
+++ sample_app/main.cc
@@ -1,6 +1,12 @@
+#include <zephyr/kernel.h>
 #include <pw_log/log.h>
 
 int main() {
-  PW_LOG_INFO("Hello World!");
+  PW_LOG_WARN("Hello World!!!");
+  while (true) {
+    k_sleep(K_SECONDS(1));
+    PW_LOG_INFO("Another second passed...");
+  }
+
   return 0;
 }
