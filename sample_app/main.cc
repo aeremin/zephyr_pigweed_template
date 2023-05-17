a1dd17411e3528eb917ba80c44d2839704eb49a3
Alexey Eremin <a.eremin.msu@gmail.com>
2023-05-18 00:14:08 +0200

    Add an example of pw_thread.sleep and pw_chrono.system_clock use.

--- sample_app/main.cc
+++ sample_app/main.cc
@@ -1,10 +1,16 @@
+#include <chrono>
+
 #include <zephyr/kernel.h>
+
+#include <pw_chrono/system_clock.h>
 #include <pw_log/log.h>
+#include <pw_thread/sleep.h>
 
 int main() {
   PW_LOG_WARN("Hello World!!!");
   while (true) {
-    k_sleep(K_SECONDS(1));
+    pw::this_thread::sleep_for(
+      pw::chrono::SystemClock::for_at_least(std::chrono::seconds(1)));
     PW_LOG_INFO("Another second passed...");
   }
 
